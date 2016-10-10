c8194d80d7169d81f003e51325876a9b58ec3d8b
Terje "Blue3agle" Monsen <registrations@tregt.net>
2016-10-10 04:30:25 +0200

    Hit messages parsed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -26,14 +26,17 @@ void MainWindow::updateCaption(void)
     }
     ui->label->setText("Open");
 
-    QString hopp = inFile.readLine();
-    if(!hopp.isEmpty())
-        theLogParser.parseLine(hopp.toStdString());
-    ui->label->setText(ui->label->text() + hopp);
+//    QString hopp = inFile.readLine();
+//    if(!hopp.isEmpty())
+//        theLogParser.parseLine(hopp.toStdString());
+//    ui->label->setText(ui->label->text() + hopp);
 }
 
 void MainWindow::on_pushButton_clicked()
 {
+    theLogParser.parseLine("[\"#000000004200000a#\",\"Other hit by other\",\"\",1475957541]Zagam hit Corrupted Hiisi Berserker for 2298 points of energy damage.");
+    theLogParser.parseLine("[\"#0000000042000008#\",\"You hit other\",\"\",1475957540]You hit The Beast for 4274 points of poison damage. Critical hit!");
+/*
     if(inFile.isOpen())
         inFile.close();
 
@@ -41,4 +44,5 @@ void MainWindow::on_pushButton_clicked()
     if (!inFile.open(QIODevice::ReadOnly | QIODevice::Text))
         return;
     inFile.seek(inFile.size());
+*/
 }
--- t_parser.cpp
+++ t_parser.cpp
@@ -1,13 +1,49 @@
 #include "t_parser.h"
 
-#include <QRegExp>
+#include <QRegularExpression>
+#include <QRegularExpressionMatch>
 #include <iostream>
 
 t_parser::t_parser()
 {
+
 }
 
-void t_parser::parseLine(const std::string &theLine)
+void t_parser::parseLine(const std::string &logLine)
 {
-    std::cout << theLine << std::endl;
+    std::cout << logLine << std::endl;
+
+    QRegularExpression timeChannelRegex(".+\"(.+)\",\"\",(\\d{10})](.+)");
+    QRegularExpressionMatch match = timeChannelRegex.match(QString::fromStdString(logLine));
+
+    if (match.hasMatch()){
+        channel = match.captured(1);
+        timestamp = match.captured(2).toInt();
+        message = match.captured(3);
+
+        for (int i = 0; i <= match.lastCapturedIndex(); ++i) {
+            std::cout << i << ": " << timestamp << " - " << channel.toStdString() << " - " << message.toStdString() << std::endl;
+        }
+        if (channel=="Other hit by other"||channel=="You hit other")
+        {
+                QRegularExpression missMessage("(?<source>.+) tried to hit (?<target>.+) , but missed.");
+                QRegularExpressionMatch missMatch = missMessage.match(message);
+                //if (!missMatch.hasMatch())
+                QRegularExpression hitMessage("(?<source>.+) hit (?<target>.+) for (?<damage>\\d+) points of (?<damageType>\\w+) damage.(?<hitType>.*)");
+                QRegularExpressionMatch hitMatch = hitMessage.match(message);
+                for (int i = 0; i <= hitMatch.lastCapturedIndex(); ++i) {
+                    std::cout << i << ": " << hitMatch.captured((i)).toStdString() << std::endl;
+                }
+                source = hitMatch.captured("source");
+                target = hitMatch.captured("target");
+                value = hitMatch.captured("damage").toInt();
+                damageType = hitMatch.captured("damageType");
+
+            if (source == "You")
+                source = player;
+        }
+        std::cout << source.toStdString() << " hit " << target.toStdString() << " for " << value << " points of " << damageType.toStdString() << " damage" << std::endl;
+
+    }
+
 }
