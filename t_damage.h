458814f3883d882406f2bbb3cc2bdc7dc40550bb
Kolbjorn Fredheim <coolbeer1980@gmail.com>
2016-10-10 19:18:18 +0200

    Added functions to set values in t_damage class

--- t_damage.h
+++ t_damage.h
@@ -8,6 +8,7 @@ namespace k_dmg
 {
     enum type{ melee, projectile, energy, disease, chemical, radiation, cold, fire, fling, burst, fullAuto, unknown };
 }
+
 namespace k_hit
 {
     enum type{ normal, glancing, critical, missed };
@@ -17,6 +18,12 @@ class t_damage
 {
     public:
         t_damage();
+        void setTimeStamp(const uint64_t &tStamp);
+        void setDamageValue(const uint32_t &dVal);
+        void setVictim(const std::string &vic);
+        void setDamageType(const std::string &damType);
+        void setHitType(const std::string &hitType);
+
         uint64_t timeStamp = 0;
         uint32_t value = 0;
         std::string victim = "";
