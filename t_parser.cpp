84d187e0f886a49e5e7881c33358f999fb261d13
Kolbjorn Fredheim <coolbeer1980@gmail.com>
2016-10-10 02:01:54 +0200

    Work in progress, keep moving!

---
+++ t_parser.cpp
@@ -0,0 +1,15 @@
+#include "t_parser.h"
+
+#include <QRegExp>
+#include <iostream>
+t_parser::t_parser()
+{
+
+}
+
+
+void t_parser::parseLine(const std::string &theLine)
+{
+
+}
+
---
+++ t_parser.h
@@ -0,0 +1,12 @@
+#ifndef T_PARSER_H
+#define T_PARSER_H
+
+
+class t_parser
+{
+public:
+    t_parser();
+    void parseLine(const std::string &theLine);
+};
+
+#endif // T_PARSER_H
