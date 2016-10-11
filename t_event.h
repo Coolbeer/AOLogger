a4695b053fe10c9f365d2e46f6c70d97e93f5331
Kolbjorn Fredheim <coolbeer1980@gmail.com>
2016-10-11 18:59:35 +0200

    Add setSource/setTarget in t_event class

--- t_event.h
+++ t_event.h
@@ -12,13 +12,14 @@ class t_event
 {
     public:
         t_event();
-        virtual k_event::k_type type(void) = 0;
-        std::string getSource(void);
-        std::string getTarget(void);
+        virtual k_event::k_type     type(void) = 0;
+        std::string                 getSource(void);
+        std::string                 getTarget(void);
+        void                        setSource(const std::string &newSource);
+        void                        setTarget(const std::string &newTarget);
     private:
-        std::string source;
-        std::string target;
-
+        std::string                 source;
+        std::string                 target;
 };
 
 #endif // T_EVENT_H
