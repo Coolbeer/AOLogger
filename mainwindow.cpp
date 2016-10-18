d811a7a06b238e80e1d626974a67396e5a3f0dc5
Blue3agle <registrations@tregt.net>
2016-10-18 19:49:06 +0200

    Saving path and player name in QSettings module.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,14 +2,21 @@
 #include "ui_mainwindow.h"
 
 #include <QTimer>
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
+    this->configuration = new QSettings(QSettings::IniFormat, QSettings::UserScope, "PWAN", "AOLogger");
+
     QTimer *timer = new QTimer();
     connect(timer, SIGNAL(timeout()), this, SLOT(updateCaption()));
+
     timer->start(1000);
+
+    ui->logPath->setText(this->configuration->value("player/logPath", "%LOCALAPPDATA%\\Funcom\\Anarchy Online").toString());
+    ui->playerName->setText(this->configuration->value("player/name", "Playername").toString());
 }
 
 MainWindow::~MainWindow()
@@ -43,9 +50,17 @@ void MainWindow::on_pushButton_clicked()
     if(inFile.isOpen())
         inFile.close();
 
-    inFile.setFileName(ui->lineEdit->text());
+    inFile.setFileName(ui->logPath->text());
     if (!inFile.open(QIODevice::ReadOnly | QIODevice::Text))
         return;
     inFile.seek(inFile.size());
-    entities.setPlayerName(ui->lineEdit_2->text().toStdString());
+    entities.setPlayerName(ui->playerName->text().toStdString());
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    this->configuration->setValue("player/logPath", ui->logPath->text());
+    this->configuration->setValue("player/name", ui->playerName->text());
+    this->configuration->sync();
+    std::cout << "Settings saved" << std::endl;
 }
