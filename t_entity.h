9a93f57217f6dca21ad2ba272c5e155fdee2001a
Blue3agle <registrations@tregt.net>
2016-10-18 18:51:26 +0200

    Resolved merge conflicts from last pull.

--- t_entity.h
+++ t_entity.h
@@ -11,27 +11,18 @@
 class t_entity
 {
     public:
-        t_entity();
-<<<<<<< HEAD
-        t_entity(std::string);
+                                t_entity();
+                                t_entity(std::string);
+        virtual                 ~t_entity(void) { };
 
-        std::string name;
-        std::vector<t_damage> damageGivenList;
-        std::vector<t_damage> damageTakenList;
-//        std::vector<t_heal> healGiven;
-//        std::vector<t_heal> healReceived;
-//        std::vector<t_loot> lootList;
-//        std::vector<t_error> errorList;
-=======
         void                    addEvent(t_event *theEvent);
         std::string             getName(void);
         void                    setName(const std::string &newName);
-        virtual                 ~t_entity(void) { };
-    private:
+
+private:
         std::string             name;
         std::vector<t_event*>   eventList;
         t_damageContainer       totalDamage;
->>>>>>> origin/master
 
 };
 
